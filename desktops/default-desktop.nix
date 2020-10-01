fdf4753997c5a227ed7988312fb3721abdba795a
kczulko <karol.czulkowski@protonmail.com>
2020-10-02 00:14:50 +0200

    Fix i3lock-pixeled.

--- desktops/default-desktop.nix
+++ desktops/default-desktop.nix
@@ -6,6 +6,13 @@ let
     fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz
     ){ config = { allowUnfree = true; }; };
 
+  i3lock-pixeled-fixed = pkgs.i3lock-pixeled.overrideDerivation (oldAttrs: rec {
+    patchPhase = oldAttrs.patchPhase + ''
+      substituteInPlace i3lock-pixeled \
+        --replace '# take the actual screenshot' 'rm $IMGFILE 2> /dev/null'
+    '';
+  });
+
   in
 {
   services.xserver = {
@@ -22,7 +29,7 @@ let
       extraPackages = with pkgs; [
         dmenu
         feh
-        i3lock-pixeled
+        i3lock-pixeled-fixed
         i3status
         polybarFull
         rofi
